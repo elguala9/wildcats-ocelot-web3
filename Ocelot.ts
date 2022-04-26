f23bc94ef101fcfbcc087dc4be78234b31a3c747
Luca Gualandi <luca.gualandi96@gmail.com>
2022-04-26 15:28:16 +0200

    Fixed the from:

--- Ocelot.ts
+++ Ocelot.ts
@@ -1,21 +1,27 @@
 import Web3 from "web3";
 const fs = require('fs');
+import Contract from 'web3-eth-contract';
 
 export class Ocelot{
     private config_path = "./config.json"
     private abi_path = "./abi.json"
     private CONFIG;
     private web3 : Web3;
-    private smart_contract;
+    private smart_contract : Contract.Contract;
+    private account : string;
 
-    constructor(provider : any){
+    constructor(provider : any, account : string){
         
         this.web3 = new Web3(provider);
+        //this.web3.eth.defaultAccount = account;
+        this.account = account;
         let content = fs.readFileSync(this.config_path);
         this.CONFIG = JSON.parse(content);
         content = fs.readFileSync(this.abi_path);
         let abi = JSON.parse(content);
+
         this.smart_contract = new this.web3.eth.Contract(abi,this.CONFIG.CONTRACT_ADDRESS);
+        //this.smart_contract.defaultAccount = account;
         //console.log(this.smart_contract);
     }
 
@@ -72,19 +78,26 @@ export class Ocelot{
     }
 
     //configuration of the transction that is used when we start a mint transaction
-    private transactionMintConfig() : any{
+    private async transactionMintConfig(){
+      //console.log(this.smart_contract.defaultAccount);
+      //console.log(this.account);
+      let price = await this.getPrice().then();
+      
       return {
         gasLimit: String(this.CONFIG.GAS_LIMIT),
         to: this.CONFIG.CONTRACT_ADDRESS,
-        value: this.getPrice(),
+        from : this.account,
+        value: price
       };
     }
 
-    public mintOcelot(): any{
-
+    public async mintOcelot(){
+      
+      let config = await this.transactionMintConfig()
+      
       this.smart_contract.methods
       .mintOcelot()
-      .send(this.transactionMintConfig())
+      .send(config)
       .once("error", (err : any) => {
         console.log(err);
         return "Sorry, something went wrong please try again later.";
